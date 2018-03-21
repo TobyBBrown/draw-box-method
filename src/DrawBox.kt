980c89418c344b9024e26f7a6a74d03f9949e781
Toby Brown <tbrown08@dcs.bbk.ac.uk>
2018-03-21 10:29:38 +0000

    Fixed drawBox for incorrect arguments.

--- src/DrawBox.kt
+++ src/DrawBox.kt
@@ -1,14 +1,16 @@
 fun drawBox(width: Int, height: Int): String {
     var box = ""
-    val horizontalEdges = " - ".repeat(width -2)
-    val topRow = "\u250c $horizontalEdges \u2510\n"
-    val bottomRow = "\u2514 $horizontalEdges \u2518\n"
-    val middleRows = "\u2502 " + "   ".repeat(width - 2) + " \u2502\n"
-    for (i in 1..height) {
-        box += when (i) {
-            1 -> topRow
-            height -> bottomRow
-            else -> middleRows
+    if (width > 1 && height > 1) {
+        val horizontalEdges = " - ".repeat(width - 2)
+        val topRow = "\u250c $horizontalEdges \u2510\n"
+        val bottomRow = "\u2514 $horizontalEdges \u2518\n"
+        val middleRows = "\u2502 " + "   ".repeat(width - 2) + " \u2502\n"
+        for (i in 1..height) {
+            box += when (i) {
+                1 -> topRow
+                height -> bottomRow
+                else -> middleRows
+            }
         }
     }
     return box
