60af426645083cefa1fcb78df07a221159ac7588
Toby Brown <tbrown08@dcs.bbk.ac.uk>
2018-03-21 10:13:47 +0000

    Added drawBox horizontal rectangle test.

--- tests/DrawBoxKtTest.kt
+++ tests/DrawBoxKtTest.kt
@@ -26,4 +26,13 @@ internal class DrawBoxKtTest {
         assertEquals(expected, drawBox(4, 8))
     }
 
+    @Test
+    fun drawBoxHoriRectangle() {
+        val expected = "\u250c  -  -  -  -  -  -  \u2510" +
+                "\n\u2502                    \u2502" +
+                "\n\u2502                    \u2502" +
+                "\n\u2514  -  -  -  -  -  -  \u2518\n"
+        assertEquals(expected, drawBox(8, 4))
+    }
+
 }
