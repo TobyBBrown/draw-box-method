e7a6e2c6e81d9313154a04c4efc810d60941266e
Toby Brown <tbrown08@dcs.bbk.ac.uk>
2018-03-21 10:05:27 +0000

    Added drawBox body.

--- src/DrawBox.kt
+++ src/DrawBox.kt
@@ -1,3 +1,16 @@
-fun drawBox(width: Int, height: Int) {
-
+fun drawBox(width: Int, height: Int): String {
+    var box = ""
+    val horizontalEdges = " - ".repeat(width -2)
+    val topRow = "\u250c $horizontalEdges \u2510\n"
+    val bottomRow = "\u2514 $horizontalEdges \u2518\n"
+    val middleRows = "\u2502 " + "   ".repeat(width - 2) + " \u2502\n"
+    for (i in 1..height) {
+        if (i == 1)
+            box += topRow
+        else if (i == height)
+            box += bottomRow
+        else
+            box += middleRows
+    }
+    return box
 }
